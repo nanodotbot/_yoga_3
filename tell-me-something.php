82994b50bc5330e58742b118a80cccbf75e64d6a
nanodotbot <nanodotbot@gmail.com>
2023-11-20 19:53:24 +0100

    just a test

--- tell-me-something.php
+++ tell-me-something.php
@@ -2,7 +2,7 @@
 // TODO: add to productive
 $ip = $_SERVER['REMOTE_ADDR']	;
 $mail_message = wordwrap($ip, 70);
-mail('info@nano.sx', 'angi.yoga', $mail_message);
+mail('info@nano.sx', 'yoga 3', $mail_message);
 
 echo '{"message" : "delivered"}';
 ?>
