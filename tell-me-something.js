74f054d4e58c804da6254e282011769674b545c0
nanodotbot <nanodotbot@gmail.com>
2023-11-20 20:08:06 +0100

    test

--- tell-me-something.js
+++
@@ -1,9 +0,0 @@
-const tellMeSomething = async () => {
-    let response = await fetch('./tell-me-something.php');
-    if (response.ok) {
-        response = await response.json();
-        console.log(response);
-    }
-}
-tellMeSomething();
-
--- tell-me-something.php
+++
@@ -1,8 +0,0 @@
-<?php 
-// TODO: add to productive
-$ip = $_SERVER['REMOTE_ADDR']	;
-$mail_message = wordwrap($ip, 70);
-mail('info@nano.sx', 'yoga 3', $mail_message);
-
-echo '{"message" : "delivered"}';
-?>
